14018c977865dfbe8d9b3a426c27b36de197ed69
Jay Baek <jbaek.01@gmail.com>
2022-05-24 08:50:46 -1000

    add basic paddle & ball interaction

--- game.py
+++ game.py
@@ -24,37 +24,34 @@ ball_width, ball_height = 20, 20
 ball = Ball(ball_width, ball_height, Color('blue'))
 ball.rect.x = 0
 ball.rect.y = SCREEN_HEIGHT-100
+ball_motion = [3,-3]
 
+# Bricks
 brick_width, brick_height = 100, 50
 columns = 6
 gap = 20
 gap_brick_combo = 120
 x_coord_counter = 100
-
 brick_row_1 = list()
-
 for col in range(columns):
     brick_row_1.append(Brick(100, 50, Color('green')))
-
 for i in brick_row_1:
     i.rect.x = x_coord_counter
     i.rect.y = SCREEN_HEIGHT/10
     x_coord_counter += gap_brick_combo
 
-# brick1 = Brick(100, 50, Color('green'))
-# brick1.rect.x = 100
-# brick1.rect.y = SCREEN_HEIGHT/10
+# Non-ball sprites list
+brick_sprites_list = pygame.sprite.Group()
+for i in brick_row_1:
+    brick_sprites_list.add(i)
 
+
+# All sprites list
 all_sprites_list = pygame.sprite.Group()
-
-all_sprites_list.add(paddle)
-# all_sprites_list.add(brick1)
 all_sprites_list.add(ball)
+all_sprites_list.add(paddle)
 for i in brick_row_1:
     all_sprites_list.add(i)
-
-ball_motion = [3,-3]
-
 
 running = True
 paused = False
@@ -81,10 +78,22 @@ while running:
         # Paddle Movement
         elif event.type == MOUSEMOTION:
             paddle.rect.x = event.pos[0] - (paddle_width/2)
+            # if ball.rect.colliderect(paddle.rect):
+            #     ball_motion[0] *= -1
+            #     ball_motion[1] *= -1
+            #     break
             if paddle.rect.x <= 0:
                 paddle.rect.x = 0
             elif paddle.rect.x >= SCREEN_WIDTH - paddle_width:
                 paddle.rect.x = SCREEN_WIDTH - paddle_width
+
+
+
+    # Ball collision
+    brick_collide_list = pygame.sprite.spritecollide(ball, brick_sprites_list, False)
+    for num, obj in enumerate(brick_collide_list):
+        print(f"{num} : COLLISION W BALL: {obj}")
+
 
     # Ball movement
     if ball.rect.right > SCREEN_WIDTH:
@@ -95,6 +104,10 @@ while running:
         ball_motion[1] *= -1
     # elif ball.rect.bottom > SCREEN_HEIGHT - 70:
     #     ball_motion[1] *= -1
+    if ball.rect.colliderect(paddle.rect):
+        # ball_motion[0] *= -1
+        ball_motion[1] *= -1
+
 
     ball.rect.move_ip(ball_motion)
 
